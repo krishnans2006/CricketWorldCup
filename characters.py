be86881280ac5d4eedb63d0d5ad01f7c2850488f
KrishnanS2006 <62958782+KrishnanS2006@users.noreply.github.com>
2020-06-08 18:22:34 -0400

    Added Files With Upload

---
+++ characters.py
@@ -0,0 +1,47 @@
+from pygame.transform import scale
+from pygame.image import load
+
+class Player:
+	imgs = [
+		scale(load("player1.png"), (128, 128)),
+		scale(load("player2.png"), (128, 128)),
+		scale(load("player3.png"), (64, 64))
+	]
+
+	def __init__(self, x, y):
+		self.x = x
+		self.y = y
+		self.swinging = False
+		self.swing_cnt = None
+		self.img = self.imgs[0]
+		self.swing_time = 30
+
+	def update(self):
+		if self.swinging:
+			self.swing_cnt += 1
+		if self.swing_cnt == self.swing_time / 2:
+			self.img = self.imgs[2]
+		if self.swing_cnt == self.swing_time:
+			self.stop_swing()
+
+	def draw(self, win):
+		win.blit(self.img, (self.x, self.y))
+
+	def swing(self):
+		if not self.swinging:
+			self.swinging = True
+			self.swing_cnt = 1
+			self.img = self.imgs[1]
+			print("Swing!")
+
+	def stop_swing(self):
+		self.swinging = False
+		self.swing_cnt = 0
+		self.img = self.imgs[0]
+
+
+class Bowler:
+	imgs = [
+		load("bowler1.png"),
+		load("bowler2.png")
+	]
---
+++ main.py
@@ -0,0 +1,46 @@
+# Made by Krishnan Shankar and Shivam Suri
+# Enjoy!
+
+import pygame
+from characters import Player  # , Bowler
+
+pygame.init()
+pygame.font.init()
+clock = pygame.time.Clock()
+
+W = 800
+H = 600
+win = pygame.display.set_mode((W, H))
+pygame.display.set_caption("Cricket Game")
+BG = pygame.transform.scale(pygame.image.load("bg.jpg"), (W, H))
+PITCH = pygame.transform.scale(pygame.image.load("pitch.png"), (300, 200))
+
+
+def redraw(win, player):
+	win.blit(BG, (0, 0))
+	win.blit(PITCH, (250, 400))
+	player.update()
+	player.draw(win)  
+	pygame.draw.line(win, (0, 0, 0), (379, 555), (379, 597), 6)
+	pygame.draw.line(win, (0, 0, 0), (399, 550), (399, 597), 6)
+	pygame.draw.line(win, (0, 0, 0), (419, 555), (419, 597), 6)
+	pygame.draw.line(win, (0, 0, 0), (380, 550), (398, 550), 4)
+	pygame.draw.line(win, (0, 0, 0), (400, 550), (418, 550), 4)
+	pygame.display.flip()
+
+
+def main():
+	player = Player(290, 460)
+	while True:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				pygame.quit()
+				quit()
+			if event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_SPACE:
+					player.swing()
+		redraw(win, player)
+		clock.tick(30)
+
+if __name__ == "__main__":
+	main()
